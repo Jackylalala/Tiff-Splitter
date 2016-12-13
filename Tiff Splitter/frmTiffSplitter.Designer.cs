744ddbfa89eb311ebc3bb119ee1fb39d3bc84143
Jackylalala <jackylalala2@gmail.com>
2016-12-14 00:24:19 +0800

    Add project files.

---
+++ Tiff Splitter/frmTiffSplitter.Designer.cs
@@ -0,0 +1,189 @@
+﻿namespace Tiff_Splitter
+{
+    partial class frmTiffSplitter
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSrc = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtSrc = new System.Windows.Forms.TextBox();
+            this.txtDst = new System.Windows.Forms.TextBox();
+            this.btnDst = new System.Windows.Forms.Button();
+            this.btnToTiff = new System.Windows.Forms.Button();
+            this.btnToPdf = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.bgdWorker = new System.ComponentModel.BackgroundWorker();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSrc
+            // 
+            this.btnSrc.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnSrc.Location = new System.Drawing.Point(327, 6);
+            this.btnSrc.Name = "btnSrc";
+            this.btnSrc.Size = new System.Drawing.Size(36, 24);
+            this.btnSrc.TabIndex = 0;
+            this.btnSrc.Text = "...";
+            this.btnSrc.UseVisualStyleBackColor = true;
+            this.btnSrc.Click += new System.EventHandler(this.btnSrc_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(52, 10);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Source file:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.Location = new System.Drawing.Point(12, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(112, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Destination path:";
+            // 
+            // txtSrc
+            // 
+            this.txtSrc.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtSrc.Location = new System.Drawing.Point(130, 7);
+            this.txtSrc.Name = "txtSrc";
+            this.txtSrc.ReadOnly = true;
+            this.txtSrc.Size = new System.Drawing.Size(191, 25);
+            this.txtSrc.TabIndex = 3;
+            // 
+            // txtDst
+            // 
+            this.txtDst.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.txtDst.Location = new System.Drawing.Point(130, 36);
+            this.txtDst.Name = "txtDst";
+            this.txtDst.ReadOnly = true;
+            this.txtDst.Size = new System.Drawing.Size(191, 25);
+            this.txtDst.TabIndex = 4;
+            // 
+            // btnDst
+            // 
+            this.btnDst.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnDst.Location = new System.Drawing.Point(327, 36);
+            this.btnDst.Name = "btnDst";
+            this.btnDst.Size = new System.Drawing.Size(36, 24);
+            this.btnDst.TabIndex = 5;
+            this.btnDst.Text = "...";
+            this.btnDst.UseVisualStyleBackColor = true;
+            this.btnDst.Click += new System.EventHandler(this.btnDst_Click);
+            // 
+            // btnToTiff
+            // 
+            this.btnToTiff.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnToTiff.Location = new System.Drawing.Point(12, 67);
+            this.btnToTiff.Name = "btnToTiff";
+            this.btnToTiff.Size = new System.Drawing.Size(171, 30);
+            this.btnToTiff.TabIndex = 6;
+            this.btnToTiff.Tag = "Tif";
+            this.btnToTiff.Text = "To Tiff";
+            this.btnToTiff.UseVisualStyleBackColor = true;
+            this.btnToTiff.Click += new System.EventHandler(this.Start);
+            // 
+            // btnToPdf
+            // 
+            this.btnToPdf.Font = new System.Drawing.Font("微軟正黑體", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.btnToPdf.Location = new System.Drawing.Point(192, 67);
+            this.btnToPdf.Name = "btnToPdf";
+            this.btnToPdf.Size = new System.Drawing.Size(171, 30);
+            this.btnToPdf.TabIndex = 7;
+            this.btnToPdf.Tag = "Pdf";
+            this.btnToPdf.Text = "To Pdf";
+            this.btnToPdf.UseVisualStyleBackColor = true;
+            this.btnToPdf.Click += new System.EventHandler(this.Start);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 106);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(397, 22);
+            this.statusStrip1.TabIndex = 8;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // bgdWorker
+            // 
+            this.bgdWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.bgdWorker_DoWork);
+            this.bgdWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.bgdWorker_RunWorkerCompleted);
+            // 
+            // frmTiffSplitter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(397, 128);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnToPdf);
+            this.Controls.Add(this.btnToTiff);
+            this.Controls.Add(this.btnDst);
+            this.Controls.Add(this.txtDst);
+            this.Controls.Add(this.txtSrc);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnSrc);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.MaximizeBox = false;
+            this.Name = "frmTiffSplitter";
+            this.Text = "Tiff Splitter";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSrc;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtSrc;
+        private System.Windows.Forms.TextBox txtDst;
+        private System.Windows.Forms.Button btnDst;
+        private System.Windows.Forms.Button btnToTiff;
+        private System.Windows.Forms.Button btnToPdf;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.ComponentModel.BackgroundWorker bgdWorker;
+    }
+}
+
---
+++ Tiff Splitter/frmTiffSplitter.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using PdfSharp;
+using PdfSharp.Pdf;
+using PdfSharp.Drawing;
+using System.IO;
+
+namespace Tiff_Splitter
+{
+    public partial class frmTiffSplitter : Form
+    {
+        private string srcFileName;
+        private string dstFilePath;
+        bool success = false;
+
+        public frmTiffSplitter()
+        {
+            InitializeComponent();
+        }
+
+        private void SplitTiffImages(string sourceFilePath, string destFilePath)
+        {
+            Image image = Image.FromFile(sourceFilePath);
+            string filename = System.IO.Path.GetFileNameWithoutExtension(sourceFilePath);
+            int frames = 0;
+            Guid[] guid = image.FrameDimensionsList;
+            FrameDimension fd = new FrameDimension(guid[0]);
+            frames = image.GetFrameCount(fd);
+
+            for (int i = 0; i < frames; i++)
+            {
+                image.SelectActiveFrame(fd, i);
+                image.Save(destFilePath + @"\"+filename+"_" + i.ToString() + ".tif", System.Drawing.Imaging.ImageFormat.Tiff);
+            }
+        }
+
+        private void Start(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(srcFileName) || string.IsNullOrEmpty(dstFilePath))
+            {
+                MessageBox.Show("Please select Source and Destination");
+                return;
+            }
+            Button btnTarget = (Button)sender;
+            bgdWorker.RunWorkerAsync(new string[3] {srcFileName,dstFilePath,btnTarget.Tag.ToString()});
+            toolStripProgressBar1.Style = ProgressBarStyle.Marquee;
+        }
+
+        private void btnSrc_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Source Tiff File";
+            ofd.Filter = @"Tiff(*.tif,*.tiff)|*.tif;*.tiff";
+            try
+            {
+                if (ofd.ShowDialog() == DialogResult.OK)
+                {
+                    srcFileName = ofd.FileName;
+                    txtSrc.Text = srcFileName;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnDst_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+            if (!string.IsNullOrEmpty(srcFileName))
+                fbd.SelectedPath = System.IO.Path.GetDirectoryName(srcFileName);
+            try
+            {
+                if (fbd.ShowDialog() == DialogResult.OK)
+                {
+                    dstFilePath = fbd.SelectedPath;
+                    txtDst.Text = dstFilePath;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bgdWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                string source = (e.Argument as string[])[0];
+                string destinaton = (e.Argument as string[])[1];
+                string worktype = (e.Argument as string[])[2];
+
+                switch (worktype)
+                {
+                    case "Tif":
+                        SplitTiffImages(source, destinaton);
+                        success = true;
+                        break;
+                    case "Pdf":
+                        PdfDocument doc = new PdfDocument();
+                        Image image = Image.FromFile(source);
+                        string filename = System.IO.Path.GetFileNameWithoutExtension(source);
+                        int frames = 0;
+                        Guid[] guid = image.FrameDimensionsList;
+                        FrameDimension fd = new FrameDimension(guid[0]);
+                        frames = image.GetFrameCount(fd);
+                        for (int i=0;i<frames;i++)
+                        {
+                            doc.AddPage();
+                            XGraphics xgr = XGraphics.FromPdfPage(doc.Pages[i]);
+                            image.SelectActiveFrame(fd, i);
+                            XImage img = XImage.FromGdiPlusImage(image);
+                            doc.Pages[i].Width = img.Width;
+                            doc.Pages[i].Height = img.Height;
+                            xgr.DrawImage(img, new Rectangle(0, 0, img.Width, img.Height));
+                        }
+                        doc.Save(destinaton + "\\" + filename +".pdf");
+                        doc.Close();
+                        success = true;
+                        break;
+                }
+                /*
+                 * PdfPage[] pp = new PdfPage[frames];
+                        for (int i = 0; i < frames; i++)
+                        {
+                            pp[i] = new PdfPage();
+                            doc.AddPage(pp[i]);
+                        }
+                        Stream stream = ToStream(image,ImageFormat.Tiff);
+                        Parallel.For(0, frames, i =>
+                            {
+                                Image imageTemp = Image.FromStream(stream);
+                                XGraphics xgr = XGraphics.FromPdfPage(pp[i]);
+                                imageTemp.SelectActiveFrame(fd, i);
+                                XImage img = XImage.FromGdiPlusImage(imageTemp);
+                                xgr.DrawImage(img,new Rectangle(0,0,img.Width,img.Height));
+                                imageTemp.Dispose();
+                            }
+                        );
+                 * 
+                 * 
+                 *
+                 * 
+                 * 
+                 * 
+                 */
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void bgdWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            toolStripProgressBar1.Style = ProgressBarStyle.Blocks;
+            toolStripProgressBar1.Value = 0;
+
+            if (success)
+                MessageBox.Show("The converion successfully.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        public static Stream ToStream(Image image, ImageFormat formaw)
+        {
+            var stream = new System.IO.MemoryStream();
+            image.Save(stream, formaw);
+            stream.Position = 0;
+            return stream;
+        }
+    }
+}
